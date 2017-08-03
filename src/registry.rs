9e7cf591f3a2980505cbcf1b58bcf7927d7502a3
Christian Kauhaus <kc@flyingcircus.io>
2017-08-03 10:00:32 +0200

    Create gcroots directories only as needed
    Also add unit tests for registry.

--- src/registry.rs
+++ src/registry.rs
@@ -27,9 +27,9 @@ fn extract_hash<'a>(path: &'a Path) -> &'a [u8] {
 
 #[derive(Debug, Clone, Default)]
 pub struct GCRoots {
-    prefix: PathBuf,
-    topdir: PathBuf,
-    cwd: PathBuf,
+    prefix: PathBuf, // /nix/var/nix/gcroots/profiles/per-user/$USER
+    topdir: PathBuf, // e.g., $PREFIX/srv/www if /srv/www was scanned
+    cwd: PathBuf, // current dir when the scan was started
     seen: HashSet<PathBuf>,
 }
 
@@ -49,15 +49,16 @@ impl GCRoots {
         })
     }
 
-    fn gc_link_dir(&self, scanned: &Path) -> PathBuf {
-        let base = scanned.parent().unwrap_or(Path::new("."));
+    fn gc_link_dir<P: AsRef<Path>>(&self, scanned: P) -> PathBuf {
+        let dir = scanned.as_ref().parent().unwrap_or(Path::new("."));
         self.prefix.join(
-            self.cwd.join(base).strip_prefix("/").unwrap(),
+            self.cwd.join(dir).strip_prefix("/").unwrap(),
         )
     }
 
-    fn create_link(&mut self, dir: &Path, target: &Path) -> Result<usize> {
-        let linkname = dir.join(&OsStr::from_bytes(extract_hash(target)));
+    fn create_link<P: AsRef<Path>, T: AsRef<Path>>(&mut self, dir: P, target: T) -> Result<usize> {
+        let target = target.as_ref();
+        let linkname = dir.as_ref().join(&OsStr::from_bytes(extract_hash(target)));
         match fs::read_link(&linkname) {
             Ok(ref p) => {
                 if *p == *target {
@@ -81,6 +82,9 @@ impl GCRoots {
             }
         }
         debug!("Linking {}", linkname.display());
+        fs::create_dir_all(&dir).chain_err(|| {
+            format!("failed to create GC dir {}", dir.as_ref().display())
+        })?;
         unix::fs::symlink(target, &linkname).chain_err(|| {
             format!("failed to create symlink {}", linkname.display())
         })?;
@@ -90,10 +94,9 @@ impl GCRoots {
 
     fn register(&mut self, sp: &StorePaths) -> Result<usize> {
         let dir = self.gc_link_dir(sp.path());
-        fs::create_dir_all(&dir).chain_err(|| {
-            format!("failed to create GC roots dir {}", dir.display())
-        })?;
-        sp.iter_refs().map(|p| self.create_link(&dir, p)).sum()
+        sp.iter_refs()
+            .map(|p| self.create_link(dir.as_path(), p))
+            .sum()
     }
 
     fn cleanup(&self) -> Result<usize> {
@@ -166,7 +169,88 @@ impl Register for NullGCRoots {
 
 #[cfg(test)]
 pub mod tests {
+    extern crate tempdir;
+
+    use self::tempdir::TempDir;
+    use std::fs::{metadata, symlink_metadata};
     use super::*;
+
+    #[test]
+    fn nonexistent_startdir_should_fail() {
+        assert!(
+            GCRoots::new(
+                &Path::new("/nix/var/nix/gcroots"),
+                &env::current_dir().unwrap().join("27866/24235/20772"),
+            ).is_err()
+        )
+    }
+
+    fn _gcroots() -> (TempDir, GCRoots) {
+        let tempdir = TempDir::new("gcroots").expect("failed to create gcroots tempdir");
+        let prefix = tempdir.path().to_owned();
+        (
+            tempdir,
+            GCRoots {
+                prefix: prefix,
+                topdir: PathBuf::from("/home/user/www"),
+                cwd: PathBuf::from("/home/user"),
+                ..GCRoots::default()
+            },
+        )
+    }
+
+    #[test]
+    fn gc_link_dir() {
+        let (td, gc) = _gcroots();
+        assert_eq!(td.path().join("home/user"), gc.gc_link_dir("file2"));
+        assert_eq!(
+            td.path().join("home/user/www/d"),
+            gc.gc_link_dir("/home/user/www/d/file1")
+        );
+        assert_eq!(td.path().join("home/user/rel"), gc.gc_link_dir("rel/file3"));
+    }
+
+    fn is_symlink(p: &Path) -> bool {
+        symlink_metadata(p)
+            .expect(&format!("symlink {} does not exist", p.display()))
+            .file_type()
+            .is_symlink()
+    }
+
+    #[test]
+    fn create_link() {
+        let (td, mut gc) = _gcroots();
+        assert_eq!(
+            gc.create_link(td.path(), "/nix/store/gmy86w4020xzjw9s8qzzz0bgx8ldkhhk-e")
+                .unwrap(),
+            1
+        );
+        let exp = td.path().join("gmy86w4020xzjw9s8qzzz0bgx8ldkhhk");
+        assert!(is_symlink(&exp));
+        assert!(gc.seen.contains(&exp));
+    }
+
+    #[test]
+    fn create_link_creates_dir() {
+        let (td, mut gc) = _gcroots();
+        assert!(metadata(td.path().join("d1")).is_err());
+        assert_eq!(
+            gc.create_link(
+                td.path().join("d1"),
+                "/nix/store/gmy86w4020xzjw9s8qzzz0bgx8ldkhhk-e",
+            ).unwrap(),
+            1
+        );
+        assert!(
+            metadata(td.path().join("d1"))
+                .expect("dir d1 not created")
+                .is_dir()
+        );
+    }
+
+    /*
+     * passive GCRoots dummy to test walker/scanner
+     */
 
     #[derive(Debug, Clone)]
     pub struct FakeGCRoots {
@@ -193,4 +277,5 @@ pub mod tests {
             Ok(())
         }
     }
+
 }
